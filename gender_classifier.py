5c154b350d324a23234b0126824a2f06e2c8a69a
“Sam <sam.alsmadi@gmail.com>
2019-08-21 18:41:15 -0400

    Add gender classifier

---
+++ gender_classifier.py
@@ -0,0 +1,19 @@
+from sklearn import tree
+
+model = tree.DecisionTreeClassifier()
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+model = model.fit(X, Y)
+
+# prediction = model.predict([[190, 70, 43]])
+prediction = model.predict([[159, 55, 37]])
+
+print(prediction)
