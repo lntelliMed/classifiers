d713def02ffe133524050bf6e236f7b2c039cf6b
“Sam <sam.alsmadi@gmail.com>
2019-08-21 19:43:25 -0400

    Add sentiment analyzer

---
+++ twitter_sentiment_analyzer.py
@@ -0,0 +1,34 @@
+from textblob import TextBlob
+# import tweepy
+
+# consumer_key= 'CONSUMER_KEY_HERE'
+# consumer_secret= 'CONSUMER_SECRET_HERE'
+
+# access_token='ACCESS_TOKEN_HERE'
+# access_token_secret='ACCESS_TOKEN_SECRET_HERE'
+
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+
+# api = tweepy.API(auth)
+
+# public_tweets = api.search('Trump')
+
+# for tweet in public_tweets:
+#     print(tweet.text)
+#     analysis = TextBlob(tweet.text)
+#     print(analysis.sentiment)
+#     print("")
+
+def detect_sentiment(text):
+  threshold = 0.5
+  analysis = TextBlob(text)
+
+  if analysis.sentiment.polarity > threshold:
+    return 'Detected sentiment: 😄'
+  else:
+    return 'Detected sentiment: 🙁'
+
+
+print(detect_sentiment('I am glad this worked out!'))
+print(detect_sentiment('This is bad!'))
